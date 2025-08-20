dd92671c781c7a13828c971c68b30ff849533d00
oorbea <oriol.orbea@gmail.com>
2025-08-20 16:12:39 +0200

    turn types schema

--- resources/TurnTypes.py
+++ resources/TurnTypes.py
@@ -1,6 +1,5 @@
 from flask_smorest import Blueprint
 from flask.views import MethodView
-from marshmallow import fields
 from enums.TurnType import TurnTypeEnum
 from helpers.auth.decorators import login_required
 
@@ -10,7 +9,7 @@ blp = Blueprint('turn-types', __name__, description='Defines the types of turns 
 class TurnTypes(MethodView):
     """TurnTypes class to define the types of turns in the game."""
     @login_required
-    @blp.response(200, fields.List(fields.String()), description="List of available turn types.")
+    @blp.response(200, description="List of available turn types.")
     @blp.response(401, description="Invalid token.")
     @blp.response(403, description="You do not have permission for this operation.")
     @blp.response(500, description="Internal server error.")
