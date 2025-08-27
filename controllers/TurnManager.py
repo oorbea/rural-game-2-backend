29d65dba9a3f120c5fbbfe53100b9536f27e9563
oorbea <oriol.orbea@gmail.com>
2025-08-27 18:46:20 +0200

    challenge restrictions

--- controllers/TurnManager.py
+++ controllers/TurnManager.py
@@ -1,6 +1,9 @@
 from random import choice
+from flask import current_app
+from sqlalchemy import or_
 from controllers.GameController import ChallengeProvider
 from enums.TurnType import TurnTypeEnum
+from helpers.PlayerInfo import PlayerInfo
 from models.Challenge import Challenge
 from models.GroupChallenge import GroupChallenge
 from models.Role import Role
@@ -33,16 +36,30 @@ class TurnManager(ChallengeProvider):
                 ponderated[fractional[i][0]] += 1
         return ponderated
     
-    def _get_valid_challenges(self, challenge: type, restrictions: dict) -> list:
-        """Get valid challenges based on restrictions."""
-        if not restrictions:
-            return challenge.query.all()
-        num_males = restrictions['males']
-        num_females = restrictions['females']
-        return challenge.query.filter(
-            challenge.males <= num_males,
-            challenge.females <= num_females
-        ).all()
+    def _get_valid_challenges(self, code: str, challenge:type, restrictions: dict, player_name: str) -> list:
+        """Get valid challenges based on lobby composition and player attributes."""
+        gc = current_app.extensions['game_controller']
+        player:PlayerInfo = gc.get_player_info(code, player_name)
+
+        filters = []
+        if restrictions:
+            num_males = int(restrictions.get('males', 0))
+            num_females = int(restrictions.get('females', 0))
+            filters += [
+                challenge.males <= num_males,
+                challenge.females <= num_females,
+            ]
+
+        filters += [
+            or_(challenge.drinking.is_(False), challenge.drinking == player.drinking),
+            or_(challenge.smoking.is_(False), challenge.smoking == player.smoking),
+            or_(challenge.partner_friendly.is_(True), challenge.partner_friendly == player.partnered),
+        ]
+
+        if challenge is not SecretMission:
+            filters.append(or_(challenge.sex.is_(False), challenge.sex != player.virgin))
+
+        return challenge.query.filter(*filters).all()
     
     def get_next_challenge(self, lobby_code: str, game_state: dict, type:TurnTypeEnum|str = TurnTypeEnum.CHALLENGE) -> dict:
         if not lobby_code:
@@ -55,27 +72,29 @@ class TurnManager(ChallengeProvider):
         if isinstance(type, str):
             type = TurnTypeEnum(type)
 
+        player_name:str = game_state['order'][game_state['lobby']['current_turn']]
+
         match type:
             case TurnTypeEnum.CHALLENGE:
-                challenges:list[Challenge] = self._get_valid_challenges(Challenge, game_state.get("restrictions", {"males": 0, "females": 0}))
+                challenges:list[Challenge] = self._get_valid_challenges(lobby_code, Challenge, game_state.get("restrictions", {"males": 0, "females": 0}), player_name)
                 if not challenges:
                     raise ValueError("No valid challenges available for the current restrictions")
                 chall = choice(challenges)
                 return chall.to_dict()
             case TurnTypeEnum.GROUP_CHALLENGE:
-                group_challenges:list[GroupChallenge] = self._get_valid_challenges(GroupChallenge, game_state.get("restrictions", {"males": 0, "females": 0}))
+                group_challenges:list[GroupChallenge] = self._get_valid_challenges(lobby_code, GroupChallenge, game_state.get("restrictions", {"males": 0, "females": 0}), player_name)
                 if not group_challenges:
                     raise ValueError("No valid group challenges available for the current restrictions")
                 gro = choice(group_challenges)
                 return gro.to_dict()
             case TurnTypeEnum.SECRET_MISSION:
-                secret_missions:list[SecretMission] = self._get_valid_challenges(SecretMission, game_state.get("restrictions", {"males": 0, "females": 0}))
+                secret_missions:list[SecretMission] = self._get_valid_challenges(lobby_code, SecretMission, game_state.get("restrictions", {"males": 0, "females": 0}), player_name)
                 if not secret_missions:
                     raise ValueError("No valid secret missions available for the current restrictions")
                 secr = choice(secret_missions)
                 return secr.to_dict()
             case TurnTypeEnum.TARGET_CHALLENGE:
-                target_challenges:list[TargetChallenge] = self._get_valid_challenges(TargetChallenge, game_state.get("restrictions", {"males": 0, "females": 0}))
+                target_challenges:list[TargetChallenge] = self._get_valid_challenges(lobby_code, TargetChallenge, game_state.get("restrictions", {"males": 0, "females": 0}), player_name)
                 if not target_challenges:
                     raise ValueError("No valid target challenges available for the current restrictions")
                 targ = choice(target_challenges)
